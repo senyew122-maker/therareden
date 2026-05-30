d55f7d82c2322356307f38ff66d3288b3eb199b3
senyew122-maker <senyew122@gmail.com>
2026-05-30 21:58:43 +0800

    first version

---
+++ contact.js
@@ -0,0 +1,60 @@
+let loggedIn = false;
+
+// 登录
+function login(){
+
+    loggedIn=true;
+
+    updateAuthUI()
+}
+
+// 登出
+function logOut(){
+
+    loggedIn=false;
+
+    updateAuthUI()
+}
+
+const contactBtn=document.getElementById("contactBtn");
+
+const contactSidebar=document.getElementById("contactSidebar");
+
+const contactArrow=document.getElementById("contact-arrow");
+
+contactBtn.addEventListener("click",()=>{
+
+    contactSidebar.classList.toggle("show");
+
+    contactArrow.classList.toggle("active");
+});
+
+// 更新UI
+function updateAuthUI(){
+
+const authArea=document.getElementById("authArea");
+
+const userArea=document.getElementById("userArea");
+
+if(loggedIn){
+
+    authArea.style.display="none";
+
+    userArea.style.display="flex";
+}else{
+
+    authArea.style.display="flex";
+
+    userArea.style.display="none";
+}
+
+}
+
+function toggleProfileMenu(){
+
+    const menu=document.getElementById("profileMenu");
+
+    menu.classList.toggle("show");
+}
+
+updateAuthUI();
